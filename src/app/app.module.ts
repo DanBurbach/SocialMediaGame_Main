54ba939c7090676012c8cbdf8491fe810dd0fc04
Student <example@example.com>
2019-02-04 11:19:28 -0800

    add routing, authentication subframe, correction to package.json, and movements in app.component main files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
+// angular main import files
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// main import files
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { masterFirebaseConfig } from './api-keys';
+
+// component import files
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
+
+// authorization import files
+import { AuthGuard } from './auth-guard/auth-guard.service';
+import { AuthenticationService } from './authentication/authentication.service';
+import { LogInComponent } from './log-in/log-in.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -19,7 +30,10 @@ export const firebaseConfig = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    PrivateComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +43,7 @@ export const firebaseConfig = {
     AngularFireDatabaseModule,
     AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [AuthGuard, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// component page routing import
+import { LogInComponent } from './log-in/log-in.component';
 
 const appRoutes: Routes = [
   {
     path: '',
-    component: WelcomeComponent
+    component: LogInComponent
   }
 ];
 
