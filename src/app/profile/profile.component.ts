149f0157582b4c83e560c04d02e25056f9f2418c
Student <example@example.com>
2019-02-06 13:24:17 -0800

    add to profile css

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,14 +10,14 @@ export class ProfileComponent {
   constructor() {
 
   }
-
+  
   openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
 
-  closeNav() {
+ closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
