315e1bce4e708ff752cffc722d1d3ebd020a60c4
Student <example@example.com>
2019-02-06 14:30:13 -0800

    Move UI component into app component, navbbar on all pages, delete UI component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthenticationService } from './authentication/authentication.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ AuthenticationService ]
 })
 export class AppComponent {
-  title = 'Social Media Game title';
+  title = 'Social Media';
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
+
+  constructor(public authService: AuthenticationService,
+    private router: Router,
+  )
+    {
+      this.authService.user.subscribe(user => {
+        if (user == null) {
+          this.isLoggedIn = false;
+          this.router.navigate(['']);
+        } else {
+          this.isLoggedIn = true;
+          this.userName = user.displayName;
+          this.router.navigate(['app-private']);
+        }
+      });
+    }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
