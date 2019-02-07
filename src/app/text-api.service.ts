dac63258b0c7f9a21a0f1c580abaa977ad6731ad
Student <example@example.com>
2019-02-07 09:00:18 -0800

    Fix results display issues

--- src/app/text-api.service.ts
+++ src/app/text-api.service.ts
@@ -8,9 +8,7 @@ export class TextApiService {
   constructor(private http: Http) { }
 
   getTextPost(search: string) {
-    // //chuck norris
     return this.http.get(`https://api.chucknorris.io/jokes/search?query=${search}`)
-
   }
 
 }
